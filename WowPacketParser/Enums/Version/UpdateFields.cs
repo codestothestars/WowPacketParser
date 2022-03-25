dd92c269310ea9108871690fdd95bb635a225645
ModoX <moardox@gmail.com>
2022-03-25 18:46:39 +0100

    Added build V9_2_0_42852

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -721,6 +721,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V9_2_0_42560:
                 case ClientVersionBuild.V9_2_0_42614:
                 case ClientVersionBuild.V9_2_0_42698:
+                case ClientVersionBuild.V9_2_0_42852:
                 {
                     return "V9_2_0_42423";
                 }
