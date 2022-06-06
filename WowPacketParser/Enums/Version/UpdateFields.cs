3477a8d1cb3034e742bc3b7a80b228712ffe7111
Shauren <shauren.trinity@gmail.com>
2022-06-06 22:34:40 +0200

    Add 9.2.5.44015

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -733,6 +733,7 @@ namespace WowPacketParser.Enums.Version
                 }
                 case ClientVersionBuild.V9_2_5_43903:
                 case ClientVersionBuild.V9_2_5_43971:
+                case ClientVersionBuild.V9_2_5_44015:
                 {
                     return "V9_2_5_43903";
                 }
