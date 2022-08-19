cd1526ee4055555388c84db62fb0e2f2e9f93d4d
ModoX <moardox@gmail.com>
2022-08-19 13:30:29 +0200

    Fixed structure of SMSG_PARTY_UPDATE for 9.2.5 and 9.2.7

--- WowPacketParserModule.V8_0_1_27101/Parsers/GroupHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/GroupHandler.cs
@@ -42,7 +42,8 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
                 packet.ReadByte("Subgroup", i);
                 packet.ReadByte("Flags", i);
                 packet.ReadByte("RolesAssigned", i);
-                packet.ReadByteE<Class>("Class", i);
+                if (ClientVersion.RemovedInVersion(9, 2, 7))
+                    packet.ReadByteE<Class>("Class", i);
                 if (ClientVersion.AddedInVersion(9, 2, 7, 1, 14, 3, 2, 5, 4))
                     packet.ReadByte("FactionGroup", i);
 
