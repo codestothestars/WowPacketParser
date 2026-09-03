addb443f501a6e0e8fc18131dd1abb9196a3892e
ratkosrb <ratkomladic2@abv.bg>
2026-09-02 20:33:12 -0400

    Fix weather update exporting.

--- WowPacketParser/Parsing/Parsers/QuestHandler.cs
+++ WowPacketParser/Parsing/Parsers/QuestHandler.cs
@@ -835,7 +835,7 @@ namespace WowPacketParser.Parsing.Parsers
             if (ClientVersion.AddedInVersion(ClientVersionBuild.V3_1_2_9901))
                 packet.ReadUInt32("Unk UInt32");
 
-            if (Settings.SqlTables.quest_client_accept)
+            if (Settings.SqlTables.client_quest_accept)
             {
                 string objectGuid;
                 uint objectId;
@@ -1282,7 +1282,7 @@ namespace WowPacketParser.Parsing.Parsers
             uint id = packet.ReadUInt32<QuestId>("Quest ID");
             packet.ReadUInt32("Reward");
 
-            if (Settings.SqlTables.quest_client_complete)
+            if (Settings.SqlTables.client_quest_complete)
             {
                 string objectGuid;
                 uint objectId;
