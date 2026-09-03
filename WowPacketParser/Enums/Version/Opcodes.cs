0bc006999842b7a4deae07b16bdf739ff4d7bb53
ratkosrb <ratkomladic2@abv.bg>
2026-09-02 20:33:12 -0400

    Add build 36935.

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -515,9 +515,9 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V1_13_6_36497:
                 case ClientVersionBuild.V1_13_6_36524:
                 case ClientVersionBuild.V1_13_6_36611:
+                case ClientVersionBuild.V1_13_6_36935:
                     return ClientVersionBuild.v1_13_6_36231;
                 case ClientVersionBuild.V1_13_6_36714:
-                case ClientVersionBuild.V1_13_6_36935:
                 case ClientVersionBuild.V1_13_6_37497:
                     return ClientVersionBuild.V1_13_6_36714;
                 default:
