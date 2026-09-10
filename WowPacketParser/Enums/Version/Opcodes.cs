3ee8e88399e30fe561ce655022f47090a39b1835
ratkosrb <ratkomladic2@abv.bg>
2026-09-10 00:09:12 -0400

    Add new classic build.

--- WowPacketParser/Enums/Version/Opcodes.cs
+++ WowPacketParser/Enums/Version/Opcodes.cs
@@ -515,14 +515,13 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V1_13_6_36497:
                 case ClientVersionBuild.V1_13_6_36524:
                 case ClientVersionBuild.V1_13_6_36611:
-                case ClientVersionBuild.V1_13_6_36935:
-                    return ClientVersionBuild.v1_13_6_36231;
                 case ClientVersionBuild.V1_13_6_36714:
+                case ClientVersionBuild.V1_13_6_36935:
                 case ClientVersionBuild.V1_13_6_37497:
                 case ClientVersionBuild.V1_13_7_38386:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
-                    return ClientVersionBuild.V1_13_6_36714;
+                    return ClientVersionBuild.v1_13_6_36231;
                 default:
                     return ClientVersionBuild.V3_3_5a_12340;
             }
