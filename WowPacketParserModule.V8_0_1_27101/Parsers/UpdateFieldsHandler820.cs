0ac2569bd2ed37bebbd7ad0426b231315831d30c
ratkosrb <ratkomladic2@abv.bg>
2026-09-14 19:32:24 -0400

    Save channeled spell in creature table.

--- WowPacketParserModule.V8_0_1_27101/Parsers/UpdateFieldsHandler820.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/UpdateFieldsHandler820.cs
@@ -646,7 +646,10 @@ namespace WowPacketParserModule.V8_0_1_27101.UpdateFields.V8_2_0_30898
         {
             var data = new UnitChannel();
             data.SpellID = packet.ReadInt32("SpellID", indexes);
-            data.SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes);
+            data.SpellVisual = new SpellCastVisual
+            {
+                SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes)
+            };
             return data;
         }
 
@@ -656,7 +659,10 @@ namespace WowPacketParserModule.V8_0_1_27101.UpdateFields.V8_2_0_30898
             if (data == null)
                 data = new UnitChannel();
             data.SpellID = packet.ReadInt32("SpellID", indexes);
-            data.SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes);
+            data.SpellVisual = new SpellCastVisual
+            {
+                SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes)
+            };
             return data;
         }
 
