caf81202330fa540f0998130b419d04049ee335c
ratkosrb <ratkomladic2@abv.bg>
2026-09-13 22:38:34 -0400

    Add new tbc classic builds and fix export of some columns.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -796,6 +796,8 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V2_5_1_38988:
                 case ClientVersionBuild.V2_5_1_39170:
                 case ClientVersionBuild.V2_5_1_39475:
+                case ClientVersionBuild.V2_5_1_39603:
+                case ClientVersionBuild.V2_5_1_39640:
                 case ClientVersionBuild.V2_5_2_39570:
                 case ClientVersionBuild.V2_5_2_39618:
                     return ClientVersionBuild.V2_5_1_38707;
@@ -1041,6 +1043,8 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V2_5_1_38988:
                 case ClientVersionBuild.V2_5_1_39170:
                 case ClientVersionBuild.V2_5_1_39475:
+                case ClientVersionBuild.V2_5_1_39603:
+                case ClientVersionBuild.V2_5_1_39640:
                 case ClientVersionBuild.V2_5_2_39570:
                 case ClientVersionBuild.V2_5_2_39618:
                 {
