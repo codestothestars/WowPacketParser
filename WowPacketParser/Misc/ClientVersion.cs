6910ceba472dd9d558e357f151bcc766585a0517
ratkosrb <ratkomladic2@abv.bg>
2026-09-13 22:37:07 -0400

    Add character spells, skills and reputation exporting.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -775,6 +775,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_6_37497:
                 case ClientVersionBuild.V1_13_7_38363:
                 case ClientVersionBuild.V1_13_7_38386:
+                case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
                     return ClientVersionBuild.V1_13_2_31446;
@@ -988,6 +989,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_6_37497:
                 case ClientVersionBuild.V1_13_7_38363:
                 case ClientVersionBuild.V1_13_7_38386:
+                case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
                 {
