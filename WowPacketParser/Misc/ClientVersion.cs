cbc0d2ccb2fb376bfa00afc44e12c9162e532b1f
ratkosrb <ratkomladic2@abv.bg>
2026-09-14 19:32:23 -0400

    Add new classic builds.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -762,6 +762,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.v1_13_5_35395:
                 case ClientVersionBuild.V1_13_5_35753:
                 case ClientVersionBuild.V1_13_5_36035:
+                case ClientVersionBuild.v1_13_5_36307:
                 case ClientVersionBuild.V1_13_5_36325:
                 case ClientVersionBuild.v1_13_6_36231:
                 case ClientVersionBuild.V1_13_6_36714:
@@ -969,6 +970,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.v1_13_5_35395:
                 case ClientVersionBuild.V1_13_5_35753:
                 case ClientVersionBuild.V1_13_5_36035:
+                case ClientVersionBuild.v1_13_5_36307:
                 case ClientVersionBuild.V1_13_5_36325:
                 case ClientVersionBuild.v1_13_6_36231:
                 case ClientVersionBuild.V1_13_6_36714:
