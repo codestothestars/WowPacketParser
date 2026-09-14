4609bcabc6a532827a33c061429b11d7219fff11
ratkosrb <ratkomladic2@abv.bg>
2026-09-14 19:32:22 -0400

    Rework db guids to allow saving guids of spell casters.

--- WowPacketParser/Store/Objects/GameObject.cs
+++ WowPacketParser/Store/Objects/GameObject.cs
@@ -8,6 +8,9 @@ namespace WowPacketParser.Store.Objects
 {
     public sealed class GameObject : WoWObject, IDataModel
     {
+        public static uint GameObjectGuidCounter = 0;
+        public uint DbGuid;
+
         public IGameObjectData GameObjectData;
         public IGameObjectData GameObjectDataOriginal;
         public uint? WorldEffectID;
@@ -15,6 +18,8 @@ namespace WowPacketParser.Store.Objects
 
         public GameObject() : base()
         {
+            DbGuid = ++GameObjectGuidCounter;
+
             GameObjectData = new GameObjectData(this);
             GameObjectDataOriginal = new OriginalGameObjectData(this);
         }
