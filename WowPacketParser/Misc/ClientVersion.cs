8abf11f994bfdbb18744d9f8be0961c54ab497f9
ratkosrb <ratkomladic2@abv.bg>
2026-09-14 19:32:25 -0400

    Add aura exporting for vanilla.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -1036,5 +1036,10 @@ namespace WowPacketParser.Misc
                     return false;
             }
         }
+
+        public static bool HasAurasInUpdateFields()
+        {
+            return ClientVersion.RemovedInVersion(ClientVersionBuild.V3_0_2_9056);
+        }
     }
 }
