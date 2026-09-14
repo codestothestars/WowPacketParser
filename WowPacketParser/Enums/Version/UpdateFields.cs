099ef093c4dfdd1ff3ea20bd00de411140584763
ratkosrb <ratkomladic2@abv.bg>
2026-09-13 22:38:34 -0400

    Add build 40011 and fix SMSG_AUTH_RESPONSE.

--- WowPacketParser/Enums/Version/UpdateFields.cs
+++ WowPacketParser/Enums/Version/UpdateFields.cs
@@ -725,6 +725,7 @@ namespace WowPacketParser.Enums.Version
                 case ClientVersionBuild.V2_5_2_39570:
                 case ClientVersionBuild.V2_5_2_39618:
                 case ClientVersionBuild.V2_5_2_39926:
+                case ClientVersionBuild.V2_5_2_40011:
                 {
                     return "V2_5_2_39570";
                 }
