8a93979e3d8ed3301c7ff4eb4705445a6998109f
ratkosrb <ratkomladic2@abv.bg>
2026-09-13 22:37:07 -0400

    Opcode corrections for 1.13.4.

--- WowPacketParserModule.V1_13_2_31446/Parsers/MiscellaneousHandler.cs
+++ WowPacketParserModule.V1_13_2_31446/Parsers/MiscellaneousHandler.cs
@@ -220,15 +220,5 @@ namespace WowPacketParserModule.V1_13_2_31446.Parsers
             for (var i = 0; i < areaCount; ++i)
                 packet.ReadUInt32<AreaId>("Area", i);
         }
-
-        [Parser(Opcode.CMSG_CLIENT_PORT_GRAVEYARD)]
-        public static void HandleClientPortGraveyard(Packet packet)
-        {
-            packet.ReadByte("UnkByte");
-            Storage.ClientReleaseSpiritTimes.Add(new WowPacketParser.Store.Objects.ClientReleaseSpirit
-            {
-                UnixTimeMs = (ulong)Utilities.GetUnixTimeMsFromDateTime(packet.Time)
-            }, packet.TimeSpan);
-        }
     }
 }
