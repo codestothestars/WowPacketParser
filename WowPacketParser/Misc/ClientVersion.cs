463170802c34766a27442d1a1d1bf40eb7d55ed4
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:54:18 -0400

    Add 2.5.1 project and fix update object parsing.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -788,7 +788,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
-                case ClientVersionBuild.V2_5_1_38043:
+                    return ClientVersionBuild.V1_13_2_31446;
                 case ClientVersionBuild.V2_5_1_38707:
                 case ClientVersionBuild.V2_5_1_38741:
                 case ClientVersionBuild.V2_5_1_38757:
@@ -798,7 +798,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V2_5_1_38988:
                 case ClientVersionBuild.V2_5_1_39170:
                 case ClientVersionBuild.V2_5_1_39475:
-                    return ClientVersionBuild.V1_13_2_31446;
+                    return ClientVersionBuild.V2_5_1_38707;
                 case ClientVersionBuild.BattleNetV37165:
                     return ClientVersionBuild.BattleNetV37165;
                 case ClientVersionBuild.Zero:
@@ -822,6 +822,8 @@ namespace WowPacketParser.Misc
             {
                 case ClientVersionBuild.V1_13_2_31446:
                     return ClientVersionBuild.V8_0_1_27101;
+                case ClientVersionBuild.V2_5_1_38707:
+                    return ClientVersionBuild.V1_13_2_31446;
                 case ClientVersionBuild.V7_0_3_22248:
                     return ClientVersionBuild.V6_0_2_19033;
                 case ClientVersionBuild.V8_0_1_27101:
@@ -843,6 +845,8 @@ namespace WowPacketParser.Misc
         {
             if (IsClassicClientVersionBuild(build))
                 return ClientType.Classic;
+            if (IsBurningCrusadeClassicClientVersionBuild(build))
+                return ClientType.BurningCrusadeClassic;
             if (build >= ClientVersionBuild.V9_0_1_36216)
                 return ClientType.Shadowlands;
             if (build >= ClientVersionBuild.V8_0_1_27101)
@@ -1012,7 +1016,18 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
-                case ClientVersionBuild.V2_5_1_38043:
+                {
+                    return true;
+                }
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IsBurningCrusadeClassicClientVersionBuild(ClientVersionBuild build)
+        {
+            switch (build)
+            {
                 case ClientVersionBuild.V2_5_1_38707:
                 case ClientVersionBuild.V2_5_1_38741:
                 case ClientVersionBuild.V2_5_1_38757:
@@ -1022,9 +1037,9 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V2_5_1_38988:
                 case ClientVersionBuild.V2_5_1_39170:
                 case ClientVersionBuild.V2_5_1_39475:
-                {
-                    return true;
-                }
+                    {
+                        return true;
+                    }
                 default:
                     return false;
             }
