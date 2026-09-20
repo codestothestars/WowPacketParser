afd2ab04b3ba9a5ee087c4c564642534cfcfe4b1
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:56 -0400

    Add creature stat saving.

--- WowPacketParserModule.V1_13_2_31446/Parsers/UpdateHandler.cs
+++ WowPacketParserModule.V1_13_2_31446/Parsers/UpdateHandler.cs
@@ -26,9 +26,15 @@ namespace WowPacketParserModule.V1_13_2_31446.Parsers
                 var outOfRangeObjCount = removedObjCount - destroyedObjCount;
 
                 for (var i = 0; i < destroyedObjCount; i++)
-                    packet.ReadPackedGuid128("ObjectGUID", "Destroyed", i);
+                {
+                    WowGuid guid = packet.ReadPackedGuid128("ObjectGUID", "Destroyed", i);
+                    Storage.StoreObjectDestroyTime(guid, packet.Time);
+                }
                 for (var i = 0; i < outOfRangeObjCount; i++)
-                    packet.ReadPackedGuid128("ObjectGUID", "OutOfRange", i);
+                {
+                    WowGuid guid = packet.ReadPackedGuid128("ObjectGUID", "OutOfRange", i);
+                    Storage.StoreObjectDestroyTime(guid, packet.Time);
+                }
             }
             packet.ReadUInt32("Data size");
 
@@ -47,9 +53,16 @@ namespace WowPacketParserModule.V1_13_2_31446.Parsers
                         break;
                     }
                     case "CreateObject1":
+                    {
+                        var guid = packet.ReadPackedGuid128("Object Guid", i);
+                        Storage.StoreObjectCreate1Time(guid, packet.Time);
+                        ReadCreateObjectBlock(packet, guid, map, i);
+                        break;
+                    }
                     case "CreateObject2":
                     {
                         var guid = packet.ReadPackedGuid128("Object Guid", i);
+                        Storage.StoreObjectCreate2Time(guid, packet.Time);
                         ReadCreateObjectBlock(packet, guid, map, i);
                         break;
                     }
