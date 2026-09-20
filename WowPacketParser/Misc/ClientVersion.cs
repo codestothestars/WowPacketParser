e951ed954fd9062be3b52cf1fa429f583b84996a
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:56 -0400

    Add 1.13.5 live build.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -763,6 +763,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.v1_13_4_34600:
                 case ClientVersionBuild.v1_13_5_34713:
                 case ClientVersionBuild.v1_13_5_34911:
+                case ClientVersionBuild.v1_13_5_35000:
                 case ClientVersionBuild.V1_13_5_35100:
                 case ClientVersionBuild.V1_13_5_35186:
                 case ClientVersionBuild.V1_13_5_35753:
@@ -966,6 +967,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.v1_13_4_34600:
                 case ClientVersionBuild.v1_13_5_34713:
                 case ClientVersionBuild.v1_13_5_34911:
+                case ClientVersionBuild.v1_13_5_35000:
                 case ClientVersionBuild.V1_13_5_35100:
                 case ClientVersionBuild.V1_13_5_35186:
                 case ClientVersionBuild.V1_13_5_35753:
