2c0b3bb2085555c84076035558c963b411aeed63
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:57 -0400

    Fix guid type check for vanilla.

--- WowPacketParserModule.V9_0_1_36216/UpdateFields/V9_1_0_39185/ObjectData.cs
+++ WowPacketParserModule.V9_0_1_36216/UpdateFields/V9_1_0_39185/ObjectData.cs
@@ -8,6 +8,7 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_1_0_39185
     public class ObjectData : IObjectData
     {
         public int EntryID { get; set; }
+        public uint TypeID { get; set; }
         public uint DynamicFlags { get; set; }
         public float Scale { get; set; }
     }
