acb78145ebb37d7906e1054936a4da97eed219cc
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:57 -0400

    Export combat movement, health updates, target changes.

--- WowPacketParserModule.V8_0_1_27101/Parsers/QuestHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/QuestHandler.cs
@@ -763,7 +763,7 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
 
             int id = packet.ReadInt32("QuestID");
             int delay = packet.ReadInt32("EmoteDelay");
-            int emote = packet.ReadInt32("EmoteType");
+            var emote = (uint)packet.ReadInt32("EmoteType");
 
             for (int i = 0; i < 2; i++)
                 packet.ReadInt32("QuestFlags", i);
