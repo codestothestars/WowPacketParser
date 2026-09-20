53a0770df797fd12c5dec5a05b07b05b90767081
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:54:18 -0400

    Fix issues with vanilla sniff parsing and add new classic build.

--- WowPacketParser/Misc/ClientVersion.cs
+++ WowPacketParser/Misc/ClientVersion.cs
@@ -788,6 +788,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
+                case ClientVersionBuild.V1_13_7_39605:
                     return ClientVersionBuild.V1_13_2_31446;
                 case ClientVersionBuild.V2_5_1_38707:
                 case ClientVersionBuild.V2_5_1_38741:
@@ -1016,6 +1017,7 @@ namespace WowPacketParser.Misc
                 case ClientVersionBuild.V1_13_7_38475:
                 case ClientVersionBuild.V1_13_7_38631:
                 case ClientVersionBuild.V1_13_7_38704:
+                case ClientVersionBuild.V1_13_7_39605:
                 {
                     return true;
                 }
