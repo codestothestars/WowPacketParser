6b223482ae06ce9b016ff55e3924adde236cf575
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:56 -0400

    Improve parsing of texts, sounds and emotes.

--- WowPacketParserModule.V9_0_1_36216/UpdateFields/V9_1_0_39185/UnitData.cs
+++ WowPacketParserModule.V9_0_1_36216/UpdateFields/V9_1_0_39185/UnitData.cs
@@ -35,12 +35,14 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_1_0_39185
         public byte DisplayPower { get; set; }
         public uint OverrideDisplayPowerID { get; set; }
         public long Health { get; set; }
+        public long CurHealth => Health;
         public int[] Power { get; } = new int[6];
         public int[] MaxPower { get; } = new int[6];
         public int MaxMana => MaxPower[0];
         public float[] PowerRegenFlatModifier { get; } = new float[6];
         public float[] PowerRegenInterruptedFlatModifier { get; } = new float[6];
         public long MaxHealth { get; set; }
+        public uint HealthPercent => (uint)(((float)CurHealth / (float)MaxHealth) * 100);
         public int Level { get; set; }
         public int EffectiveLevel { get; set; }
         public int ContentTuningID { get; set; }
