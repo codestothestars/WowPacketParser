4a53f880ef171b9adbf38da987b2f9e029d17377
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:58 -0400

    Add exporting of dynamic objects.

--- WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler905.cs
+++ WowPacketParserModule.V9_0_1_36216/Parsers/UpdateFieldsHandler905.cs
@@ -3816,7 +3816,7 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_0_5_37862
         {
             var data = new DynamicObjectData();
             data.Caster = packet.ReadPackedGuid128("Caster", indexes);
-            data.SpellVisual = ReadCreateSpellCastVisual(packet, indexes, "SpellVisual");
+            data.SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes);
             data.SpellID = packet.ReadInt32("SpellID", indexes);
             data.Radius = packet.ReadSingle("Radius", indexes);
             data.CastTime = packet.ReadUInt32("CastTime", indexes);
@@ -3842,7 +3842,7 @@ namespace WowPacketParserModule.V9_0_1_36216.UpdateFields.V9_0_5_37862
                 }
                 if (changesMask[2])
                 {
-                    data.SpellVisual = ReadUpdateSpellCastVisual(packet, data.SpellVisual as SpellCastVisual, indexes, "SpellVisual");
+                    data.SpellXSpellVisualID = packet.ReadInt32("SpellXSpellVisualID", indexes);
                 }
                 if (changesMask[3])
                 {
