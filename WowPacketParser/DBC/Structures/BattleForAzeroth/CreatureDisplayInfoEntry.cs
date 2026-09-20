79e198bd69a902b367ef5ba2116fc7861bbe919c
ratkosrb <ratkomladic2@abv.bg>
2026-09-19 21:53:56 -0400

    Save uses of items and gameobject to db.

--- WowPacketParser/DBC/Structures/BattleForAzeroth/CreatureDisplayInfoEntry.cs
+++ WowPacketParser/DBC/Structures/BattleForAzeroth/CreatureDisplayInfoEntry.cs
@@ -7,7 +7,7 @@ namespace WowPacketParser.DBC.Structures.BattleForAzeroth
     {
         [Index(false)]
         public uint ID;
-        public ushort DisplayID;
+        public ushort ModelID;
         public ushort SoundID;
         public sbyte SizeClass;
         public float CreatureModelScale;
